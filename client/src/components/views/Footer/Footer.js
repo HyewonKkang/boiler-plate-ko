540a9178cf06d03b903f5d36e80b4f6fc1851dd9
HyewonKkang <68578916+HyewonKkang@users.noreply.github.com>
2021-01-23 15:15:59 +0900

    Add react part_part2 - 21/01/23

---
+++ client/src/components/views/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+function Footer() {
+    return (
+       <div className="App">
+         AppAppAppApp
+       </div>
+    )
+}
+
+export default Footer
--- server/index.js
+++ server/index.js
@@ -24,6 +24,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! hi~')
 })
 
+app.get('/api/hello', (req, res) => {
+  res.send("안녕하세요 ~")
+})
+
 app.post('/api/users/register', (req, res) => {
 
   // 회원 가입 할때 필요한 정보들을 client에 넣어주면
